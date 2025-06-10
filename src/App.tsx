098d25501b1359e70385eec3e52dae40dfeb7c90
JakobOpresnik <jakob.opresnik@gmail.com>
2025-06-10 12:52:08 +0200

    Fix import

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import StationMarker from './components/StationMarker';
 import { getStationsData } from './api/stations';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
-import iconRetinaUrl from 'leaflet/dist/images/marker-icon-2x.png';
+// import iconRetinaUrl from 'leaflet/dist/images/marker-icon-2x.png';
 import iconUrl from 'leaflet/dist/images/marker-icon.png';
 import shadowUrl from 'leaflet/dist/images/marker-shadow.png';
 
