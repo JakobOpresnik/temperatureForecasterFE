d103031e2dc308431c71054f9f2af1541dcc758d
JakobOpresnik <jakob.opresnik@gmail.com>
2025-06-10 11:10:05 +0200

    Fix env variables

--- src/api/supabaseClient.ts
+++ src/api/supabaseClient.ts
@@ -2,7 +2,10 @@ import { createClient } from '@supabase/supabase-js';
 import type { Database } from '../types/supabase';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
-const supabaseKey = import.meta.env.VITE_SUPABASE_URL as string;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY as string;
+
+console.log("SUPABASE URL: ", supabaseUrl)
+console.log("SUPABASE_KEY: ", supabaseKey)
 
 const supabase = createClient<Database>(supabaseUrl, supabaseKey);
 
